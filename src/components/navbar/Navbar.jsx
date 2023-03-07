170a89facb861f9420044c6b2d9a79566b44b92e
yusufburakkaradag42 <yusufburakkaradag42@gmail.com>
2023-03-07 22:23:19 +0300

    navbr

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
+import { Logo, Menu, MenuLink, Nav } from "./Navbar.style";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <Nav justify="space-between">
+      <Logo to="/">YBK Library</Logo>
+      <Menu>
+        <MenuLink to="/">Home</MenuLink>
+        <MenuLink to="/about">About</MenuLink>
+        <MenuLink to="/register">Register</MenuLink>
+        <MenuLink to="/login">Login</MenuLink>
+        <MenuLink to="/login">Logout</MenuLink>
+      </Menu>
+    </Nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
