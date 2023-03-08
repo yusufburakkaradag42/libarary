a22e1e03890214327c3f5c4cb3c4288e6d8236ba
yusufburakkaradag42 <yusufburakkaradag42@gmail.com>
2023-03-08 20:26:50 +0300

    header

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
-import { HomeContainer } from './Home.style'
-
+import Header from '../../components/header/Header'
+import { HomeContainer, HomeImg, MainContainer } from './Home.style'
+import homeImg from "../../assets/books.jpg"
 const Home = () => {
   return (
     <HomeContainer>
+    <Header/>
+    <HomeImg>
+      <img src={homeImg} alt="home img"/>
+    </HomeImg>
+    <MainContainer>
 
+      
+    </MainContainer>
     </HomeContainer>
   )
 }
--- src/pages/home/Home.style.jsx
+++ src/pages/home/Home.style.jsx
@@ -5,9 +5,15 @@ export const HomeContainer = styled(Flex)`
     flex-direction:column;
 `
 export const HomeImg = styled.div`
-    width: 35%;
-    max-height: 50vh;
+    margin: 2rem;
+    display: flex;
+    justify-content: center;
+    width: 100%;
+    img{
+    width: 45%;
+    height: 50vh;
     border-radius: 1rem;
+    }
 `
 export const MainContainer = styled(Flex)`
     flex-direction: row;
